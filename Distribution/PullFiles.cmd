13adf9c7e3b32f2058f0332e14d66ca1e266b72d
Rick Strahl <rstrahl@west-wind.com>
2014-04-10 01:38:25 -0600

    Update distribution files.

--- Distribution/PullFiles.cmd
+++ Distribution/PullFiles.cmd
@@ -1,2 +1,11 @@
 copy ..\Westwind.Globalization\bin\release\*.dll
 copy ..\Westwind.Globalization\bin\release\*.xml
+
+copy ..\Westwind.Globalization.Sample\bin\Westwind.Utilities.dll
+copy ..\Westwind.Globalization.Sample\bin\Westwind.Utilities.xml
+
+copy ..\Westwind.Globalization.Sample\bin\Westwind.Web.dll
+copy ..\Westwind.Globalization.Sample\bin\Westwind.Web.xml
+
+copy ..\Westwind.Globalization.Sample\bin\Westwind.Web.WebForms.dll
+copy ..\Westwind.Globalization.Sample\bin\Westwind.Web.WebForms.xml
