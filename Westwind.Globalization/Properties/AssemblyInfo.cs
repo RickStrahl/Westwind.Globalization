bef994238d36fcdaf80155d79be26b821633195f
Rick Strahl <rstrahl@west-wind.com>
2016-05-18 00:09:10 -0700

    Project Housekeeping

--- Westwind.Globalization/Properties/AssemblyInfo.cs
+++ Westwind.Globalization/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Web.UI;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.10.0")]
-[assembly: AssemblyFileVersion("2.10.0")]
+[assembly: AssemblyVersion("2.12.0")]
+[assembly: AssemblyFileVersion("2.12.0")]
