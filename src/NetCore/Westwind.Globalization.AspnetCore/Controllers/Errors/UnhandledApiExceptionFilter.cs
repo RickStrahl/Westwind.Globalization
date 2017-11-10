58635e92c950fe869c781c32bcc6c7d29ae4d876
Rick Strahl <rstrahl@west-wind.com>
2017-11-10 00:56:37 -1000

    Fix Exception filter - accidental nested class caused filter to not work at all. Errors are now properly returned.

--- src/NetCore/Westwind.Globalization.AspnetCore/Controllers/Errors/UnhandledApiExceptionFilter.cs
+++ src/NetCore/Westwind.Globalization.AspnetCore/Controllers/Errors/UnhandledApiExceptionFilter.cs
@@ -14,32 +14,29 @@ namespace Westwind.Globalization.Errors
     public class UnhandledApiExceptionFilter : ExceptionFilterAttribute
     {
 
-
-        public class ApiExceptionFilter : ExceptionFilterAttribute
+        public override void OnException(ExceptionContext context)
         {
-            public override void OnException(ExceptionContext context)
+            ApiError apiError = null;
+            if (context.Exception is ApiException)
             {
-                ApiError apiError = null;
-                if (context.Exception is ApiException)
-                {
-                    // handle explicit 'known' API errors
-                    var ex = context.Exception as ApiException;
-                    context.Exception = null;
-                    apiError = new ApiError(ex.Message);
-                    apiError.errors = ex.Errors;
+                // handle explicit 'known' API errors
+                var ex = context.Exception as ApiException;
+                context.Exception = null;
+                apiError = new ApiError(ex.Message);
+                apiError.errors = ex.Errors;
 
-                    context.HttpContext.Response.StatusCode = ex.StatusCode;
-                }
-                else if (context.Exception is UnauthorizedAccessException)
-                {
-                    apiError = new ApiError("Unauthorized Access");
-                    context.HttpContext.Response.StatusCode = 401;
+                context.HttpContext.Response.StatusCode = ex.StatusCode;
+            }
+            else if (context.Exception is UnauthorizedAccessException)
+            {
+                apiError = new ApiError("Unauthorized Access");
+                context.HttpContext.Response.StatusCode = 401;
 
-                    // handle logging here
-                }
-                else
-                {
-                    // Unhandled errors
+                // handle logging here
+            }
+            else
+            {
+                // Unhandled errors
 #if !DEBUG
                     var msg = "An unhandled error occurred.";
                     string stack = null;
@@ -48,20 +45,20 @@ namespace Westwind.Globalization.Errors
                 string stack = context.Exception.StackTrace;
 #endif
 
-                    apiError = new ApiError(msg);
-                    apiError.detail = stack;
+                apiError = new ApiError(msg);
+                apiError.detail = stack;
 
-                    context.HttpContext.Response.StatusCode = 500;
+                context.HttpContext.Response.StatusCode = 500;
 
-                    // handle logging here
-                }
-
-                // always return a JSON result
-                context.Result = new JsonResult(apiError);
-
-                base.OnException(context);
+                // handle logging here
             }
+
+            // always return a JSON result
+            context.Result = new JsonResult(apiError);
+
+            base.OnException(context);
         }
-        
     }
+
+
 }
