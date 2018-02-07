f136c7acebbfe6aa6218874f45e20973bbed262d
Steve Suehrstedt <ssuehrstedt@benefitvision.com>
2018-02-07 14:06:16 -0700

    Update .NET 4.5 package reference paths.
    Fixes build issues on fresh clone.
    Adjust home links on test pages.

--- src/Net45/Westwind.Globalization.Sample/Global.asax.cs
+++ src/Net45/Westwind.Globalization.Sample/Global.asax.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
