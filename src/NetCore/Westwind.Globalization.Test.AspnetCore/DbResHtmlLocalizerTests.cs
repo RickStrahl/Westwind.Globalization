a59bbc394caeef00a2b1acf1b05169177e5f1d72
Toni Wenzel <toni.wenzel@googlemail.com>
2018-10-19 14:21:27 +0200

    Set ResourceSet on WithCulture Fixes #138

---
+++ src/NetCore/Westwind.Globalization.Test.AspnetCore/DbResHtmlLocalizerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Westwind.Globalization.AspnetCore;
+
+namespace Westwind.Globalization.Test.AspnetCore
+{
+    [TestFixture]
+    public class DbResHtmlLocalizerTests
+    {
+        [Test]
+        public void ResourceSetRemainsOnWithCultureCall()
+        {
+            var localizer = new DbResHtmlLocalizer(new DbResourceConfiguration()) { ResourceSet = "Test" };
+            var newCultureLocalizer = (DbResHtmlLocalizer)localizer.WithCulture(new System.Globalization.CultureInfo("en-US"));
+
+            Assert.AreEqual(localizer.ResourceSet, newCultureLocalizer.ResourceSet);
+        }
+    }
+}
---
+++ src/NetCore/Westwind.Globalization.Test.AspnetCore/DbResStringLocalizerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Westwind.Globalization.AspnetCore;
+
+namespace Westwind.Globalization.Test.AspnetCore
+{
+    [TestFixture]
+    public class DbResStringLocalizerTests
+    {
+        [Test]
+        public void ResourceSetRemainsOnWithCultureCall()
+        {
+            var localizer = new DbResStringLocalizer(new DbResourceConfiguration()) { ResourceSet = "Test" };
+            var newCultureLocalizer = (DbResStringLocalizer)localizer.WithCulture(new System.Globalization.CultureInfo("en-US"));
+
+            Assert.AreEqual(localizer.ResourceSet, newCultureLocalizer.ResourceSet);
+        }
+    }
+}
